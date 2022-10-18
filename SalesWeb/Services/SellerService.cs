61c33b602d8e120bf013904daf43d1ccd5b53f58
Aureo-Bueno <aureoalexandre@outlook.com>
2022-10-18 14:05:22 -0300

    feat: adiciona exception

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -36,9 +36,17 @@ namespace SalesWeb.Services
 
         public async Task RemoveAsync(int id)
         {
-            var idSeller = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(idSeller);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var idSeller = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(idSeller);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+
+                throw new IntegrityException("Can`t delete seller because he/she has sales!");
+            }
         }
 
         public async Task UpdateAsync(Seller seller)
