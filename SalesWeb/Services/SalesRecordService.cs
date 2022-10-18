6f05bb35d93e84f10eea6ef5f8a276352dbddb0b
Aureo-Bueno <aureoalexandre@outlook.com>
2022-10-18 15:19:11 -0300

    feat: adiciona busca simples

---
+++ SalesWeb/Services/SalesRecordService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWeb.Data;
+using SalesWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWeb.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebContext _context;
+
+        public SalesRecordService(SalesWebContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(w => w.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(w => w.Date <= maxDate.Value);
+            }
+
+            return await result.Include(i => i.Seller).Include(i => i.Seller.Department).OrderByDescending(o => o.Date).ToListAsync();
+        }
+    }
+}
