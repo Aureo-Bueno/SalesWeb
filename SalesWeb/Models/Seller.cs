039b84e5474a0d7401c21b950cb7d486dd23c208
Aureo-Bueno <aureoalexandre@outlook.com>
2022-10-18 08:38:35 -0300

    feat: adiciona validacoes nos campos dos formularios

--- SalesWeb/Models/Seller.cs
+++ SalesWeb/Models/Seller.cs
@@ -8,17 +8,25 @@ namespace SalesWeb.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required!")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}!")]
         public string Name { get; set; }
 
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required!")]
+        [EmailAddress(ErrorMessage = "Enter a valid email!")]
         public string Email { get; set; }
 
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} required!")]
         public DateTime BirthDate { get; set; }
 
         [Display(Name = "Base Salary")]
+        [Required(ErrorMessage = "{0} required!")]
+        [Range(100.0, 50000.00, ErrorMessage = "{0} must be from {1} to {2}!")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
